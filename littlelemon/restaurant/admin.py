6d008bf0e93aad7f7454c081486e799991e545e5
MuqsitX <alhajimuhammadisah@gmail.com>
2023-07-25 22:50:29 +0100

    completion of littlelemon

---
+++ littlelemon/restaurant/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+# from .models import About
+from .models import Menu
+from .models import Booking
+# from .models import Menu_item
+
+# Register your models here.
+# admin.site.register(About)
+admin.site.register(Menu)
+admin.site.register(Booking)
+# admin.site.register(Menu_item)
